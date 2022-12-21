25d22b14992b5ee80d71dd9c901adfc3d5a4edad
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-22 00:58:46 +0100

    Solve Median of two sorted arrays

---
+++ Data structure: Array/Median of Two Sorted Arrays/solution.js
@@ -0,0 +1,10 @@
+const findMedianSortedArrays = (nums1, nums2) => {
+  const nums3 = nums1.concat(nums2).sort((a, b) => a - b);
+  if (nums3.length % 2 !== 0) return nums3[Math.trunc(nums3.length / 2)];
+  if (nums3.length % 2 === 0) {
+    const i = nums3[Math.trunc(nums3.length / 2) - 1] + nums3[Math.trunc(nums3.length / 2)];
+    return i / 2;
+  }
+};
+
+findMedianSortedArrays([1, 2], [3, 4]);
