df5652650937a9d443d8519778165e1b7457f1e6
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-21 03:23:31 +0100

    Add && Fix linter

--- Data structure: Array/twoSum/solution.js
+++ Data structure: Array/twoSum/solution.js
@@ -1,7 +1,10 @@
-var twoSum = function(nums, target) {
-  for(let i = 0; i < nums.length; i++) {
-      if(nums[i] + nums [i+1] === target){
-          return [i, i+1]
-      }
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] + nums[i + 1] === target) {
+      return [i, i + 1];
+    }
   }
+  return 0;
 };
+
+twoSum([2, 7, 11, 15], 9);
