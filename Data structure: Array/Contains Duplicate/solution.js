f884d74b670082fd602b1c3a6196e2447510b3a8
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-23 19:10:52 +0100

    Fix linters

--- Data structure: Array/Contains Duplicate/solution.js
+++ Data structure: Array/Contains Duplicate/solution.js
@@ -1,6 +1,6 @@
 const containsDuplicate = (nums) => {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
       if (nums[i] === nums[j]) {
         return true;
       }
