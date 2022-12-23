83edebed4925ce02b1ae52230061a49e6844f261
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-23 21:00:46 +0100

    Solve the 3 Sum

---
+++ Data structure: Array/3Sum/solution.js
@@ -0,0 +1,15 @@
+const threeSum = (nums) => {
+  let res = [];
+  for (let i = 0; i < nums.length; i++){
+      for(let j = i + 1; j+1 < nums.length; j++){
+          if (i != j && i != j+1 && j != j+1) {
+              if (nums[i] + nums[j] + nums[j+1] == 0) {
+                  res.push([nums[i], nums[j], nums[j+1]]);
+              }
+          }
+      }
+  }
+  return res;
+};
+
+threeSum([-1,0,1,2,-1,-4]);
